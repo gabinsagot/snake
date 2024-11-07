55e1f54d42e62f002beec5045ddbd9a1eb71dc7d
Gabin Sagot <gabin.sagot@etu.minesparis.psl.eu>
2024-11-07 11:43:17 +0100

    snake

--- snake.py
+++ snake.py
@@ -1,121 +1,111 @@
-"""
-v1 : pareil mais au moins on peut sortir du programme
-avec la touche 'q', ou avec la souris en fermant la fenêtre
-"""
-
 from random import randint
 import pygame as pg
 
-pg.init()
-screen = pg.display.set_mode((600, 600))
+WIDTH = 20 # largeur du rectangle en pixels
+HEIGHT = 20 # hauteur du rectangle en pixels
+BLACK = (0, 0, 0) # couleur noire
+GREEN = (0, 255, 0) #couleur verte
+RED = (255, 0, 0)
+SCREEN_SIZE = (600, 600)
+
+screen = pg.display.set_mode(SCREEN_SIZE)
 clock = pg.time.Clock()
 count = 0
 
 
-# on rajoute une condition à la boucle: si on la passe à False le programme s'arrête
-snake = [
-        (10, 15),
-        (11, 15),
-        (12, 15),]
-direction = (1, 0)
-fruit = (10, 10)
-running = True
+def handle_events(event):
 
-while running:
-    #Création du serpent
-
-
-    screen_color = (255, 255, 255)
-    screen.fill(screen_color)
-    clock.tick(6)
-
-    # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
-    # ici donc tous les évènements survenus durant la seconde précédente
-    for event in pg.event.get():
-        # chaque évênement à un type qui décrit la nature de l'évênement
-        # un type de pg.QUIT signifie que l'on a cliqué sur la "croix" de la fenêtre
-        """
-
-        """
-        if event.type == pg.QUIT:
-            running = False
-        # un type de pg.KEYDOWN signifie que l'on a appuyé une touche du clavier
-        elif event.type == pg.KEYDOWN:
-            # si la touche est "Q" on veut quitter le programme
-            if event.key == pg.K_UP:
-                direction = (0, -1)
-            if event.key == pg.K_DOWN:
-                direction = (0, 1)
-            if event.key == pg.K_LEFT:
-                direction = (-1, 0)
-            if event.key == pg.K_RIGHT:
-                direction = (1, 0)
-            if event.key == pg.K_q:
-                running = False
-
-    # xxx ici c'est discutable, car si on tape 'q'
-    # on va quand même changer de couleur avant de sortir...
-
-    width = 20 # largeur du rectangle en pixels
-    height = 20 # hauteur du rectangle en pixels
-    black = (0, 0, 0) # couleur noire
-    green = (0, 255, 0) #couleur verte
-    red = (255, 0, 0)
-
-    #Création du damier
-    
-    for i in range(15):
-        for j in range(15):
-            x = (2*i+1)*width
-            y = 2*j*height
-
-            rect = pg.Rect(x, y, width, height)
-            pg.draw.rect(screen, black, rect)
-
-    for i in range(15):
-        for j in range(15):
-            x = 2*i*width
-            y = (2*j+1)*height
-
-            rect = pg.Rect(x, y, width, height)
-            pg.draw.rect(screen, black, rect)
-    """   
-    for event in pg.event.get():
-        if event.key == pg.K_w:
-            direction = (0, 1)
-        if event.key == pg.K_s:
-            direction = (0, -1)
-        if event.key == pg.K_a:
-            direction = (-1, 0)
-        if event.key == pg.K_d:
-            direction = (1, 0)
-"""
-    snake.append(tuple(a + b for a,b in zip(snake[-1], direction)))
-    queue = snake.pop(0)
-        
-
-    xf, yf = width*fruit[0], height*fruit[1]
-    rect_fruit = pg.Rect(xf, yf, width, height)
-    pg.draw.rect(screen, red, rect_fruit)
-
-    if snake[-1]==fruit:
-        count += 1
-        snake.insert(0, queue)
-        fruit = randint(0, 29), randint(0, 29)
-
-    if snake[-1] in snake[:-1]:
-        print(count)
+    if event.type == pg.QUIT:
         running = False
+            # un type de pg.KEYDOWN signifie que l'on a appuyé une touche du clavier
+    elif event.type == pg.KEYDOWN:
+        # si la touche est "Q" on veut quitter le programme
+        if event.key == pg.K_UP:
+            direction = (0, -1)
+        if event.key == pg.K_DOWN:
+            direction = (0, 1)
+        if event.key == pg.K_LEFT:
+            direction = (-1, 0)
+        if event.key == pg.K_RIGHT:
+            direction = (1, 0)
+        if event.key == pg.K_q:
+            running = False
 
-    for point in snake:
-        x, y = width*point[0], height*point[1]
-        rect_snake = pg.Rect(x, y, width, height)
-        pg.draw.rect(screen, green, rect_snake)
+def draw_snake(snake):
+    for pos_i, pos_j in snake:
+        rect_snake = pg.Rect(WIDTH*pos_i, HEIGHT*pos_j, WIDTH, HEIGHT)
+        pg.draw.rect(screen, GREEN, rect_snake)
+
+def eat_snake(snake, fruit, count):
+    count += 1
+    snake.insert(0, queue)
+    fruit = randint(0, 29), randint(0, 29)
 
 
-    pg.display.update()
+if __name__=="__main__":
+
+    pg.init()
+
+    # on rajoute une condition à la boucle: si on la passe à False le programme s'arrête
+    snake = [
+            (10, 15),
+            (11, 15),
+            (12, 15),]
+    direction = (1, 0)
+    fruit = (10, 10)
+    running = True
+
+    while running:
+        
+        screen_color = (255, 255, 255)
+        screen.fill(screen_color)
+        clock.tick(6)
+
+        # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
+        # ici donc tous les évènements survenus durant la seconde précédente
+        for event in pg.event.get():
+            # chaque évênement à un type qui décrit la nature de l'évênement
+            # un type de pg.QUIT signifie que l'on a cliqué sur la "croix" de la fenêtre
+            handle_events(event)
+
+        #Création du damier
+        
+        for i in range(15):
+            for j in range(15):
+                x = (2*i+1)*WIDTH
+                y = 2*j*HEIGHT
+
+                rect = pg.Rect(x, y, WIDTH, HEIGHT)
+                pg.draw.rect(screen, BLACK, rect)
+
+        for i in range(15):
+            for j in range(15):
+                x = 2*i*WIDTH
+                y = (2*j+1)*HEIGHT
+
+                rect = pg.Rect(x, y, WIDTH, HEIGHT)
+                pg.draw.rect(screen, BLACK, rect)
+
+        snake.append(tuple(a + b for a,b in zip(snake[-1], direction)))
+        queue = snake.pop(0)
+            
+
+        xf, yf = WIDTH*fruit[0], HEIGHT*fruit[1]
+        rect_fruit = pg.Rect(xf, yf, WIDTH, HEIGHT)
+        pg.draw.rect(screen, RED, rect_fruit)
+
+        if snake[-1]==fruit:
+            eat_snake(snake, fruit, count)
+
+        if snake[-1] in snake[:-1]:
+            print(count)
+            running = False
+
+        draw_snake(snake)
+
+        pg.display.update()
 
 
-# Enfin on rajoute un appel à pg.quit()
-# Cet appel va permettre à Pygame de "bien s'éteindre" et éviter des bugs sous Windows
-pg.quit()
+    # Enfin on rajoute un appel à pg.quit()
+    # Cet appel va permettre à Pygame de "bien s'éteindre" et éviter des bugs sous Windows
+    pg.quit()
